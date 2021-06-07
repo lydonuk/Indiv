24c1200717daed252e121ede6650c8013b855226
lydonuk <85514494+lydonuk@users.noreply.github.com>
2021-06-07 19:39:16 +0100

    Add files via upload

---
+++ Key-System_v1.0.py
@@ -0,0 +1,40 @@
+#Login System (v1.0)
+import os
+import stdiomask
+import platform
+
+title = 'Login System'
+OS = platform.system()
+K = 'root'
+x = 3
+
+while x > 0:
+	key = stdiomask.getpass('Key: ')
+	x = x - 1
+	
+	if key != K:
+		print('Key incorrect! Attempts left: ' + str(x))
+		
+		if x <= 0:
+			input()
+			quit()
+			
+		else:
+			input('Press enter to retry...')
+		
+			if OS == 'Windows':
+				os.system('cls')
+			
+			elif OS == 'Linux' or 'Darwin':
+				os.system('clear')
+				
+	else:
+			if OS == 'Windows':
+				os.system('cls')
+			
+			elif OS == 'Linux' or 'Darwin':
+				os.system('clear')
+			
+				break
+				
+#Add code
